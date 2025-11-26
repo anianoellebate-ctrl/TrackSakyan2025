e8fc03b07e2c67bdacdd32156e64337ee051837c
anianoellebate-ctrl <anianoellebate@gmail.com>
2025-11-27 01:55:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -31,6 +32,7 @@ const plantripRouter = require('./routes/plantrip.router');
 const congestionRouter = require('./routes/congestion.router');
 const jeepneydensityRouter = require('./routes/jeepneydensity.router');
 const forgotpasswordRouter = require('./routes/forgotpassword.router');
+const uploadRouter = require('./routes/upload.router');
 
 
 
